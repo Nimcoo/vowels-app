bd93dcb5fa49ad12a1ff62ae111361d09567d87c
NIMCOO <nimco77001@gmail.com>
2023-12-03 07:38:54 +0000

    Done Exercise

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const input = document.querySelector(".input-text").value 
+//count vowels
+document.querySelector("vowel-count").addEventListener("click",function(){
+    
+})
+if(input =vowel){
+    console.log(input)
+}else{
+    console.log(uncount)
+}
 
